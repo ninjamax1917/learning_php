46f8a99d273e09cef9698c36b36f141c35f65706
ninjamax1917 <ninjamax177@gmail.com>
2025-08-17 14:26:47 +0300

    Изменил Readme.md

--- Dockerfile
+++ Dockerfile
@@ -5,18 +5,3 @@ FROM php:8.2-fpm
 
 WORKDIR /var/www/html
 
-RUN mkdir -p /var/www/html/.vscode && \
-    printf '%s\n' '{' \
-    '  "emeraldwalk.runonsave": {' \
-    '    "commands": [' \
-    '      {' \
-    '        "match": "\\.php$", ' \
-    '        "cmd": "docker compose run --rm php ./vendor/bin/pint ${relativeFile}",' \
-    '        "silent": true' \
-    '      }' \
-    '    ]' \
-    '  },' \
-    '  "[php]": {' \
-    '    "editor.formatOnSave": false' \
-    '  }' \
-    '}' > /var/www/html/.vscode/settings.json
--- index.php
+++ index.php
@@ -2,98 +2,18 @@
 
 include_once 'vendor/autoload.php';
 
-class Route
+class Car
 {
-    public function __construct(
-        private string $uri,
-        private string $method,
-        private $action
-    ) {}
-
-    public static function get(string $uri, $action): static
+    public function drive(): string
     {
-        return new static($uri, 'GET', $action);
+
+        return 'The car is driving.';
     }
 
-    public static function post(string $uri, $action): static
-    {
-        return new static($uri, 'POST', $action);
-    }
+    public function __construct(public string $color, public string $model) {}
 
-    public function getUri(): string
-    {
-        return $this->uri;
-    }
-
-    public function getMethod(): string
-    {
-        return $this->method;
-    }
-
-    public function getAction(): callable
-    {
-        return $this->action;
-    }
 }
 
-class Router
-{
-    private $routes = [
-        'GET' => [],
-        'POST' => [],
-    ];
+$toyota = new Car('red', 'Toyota');
 
-    public function __construct()
-    {
-        $this->initRoutes();
-    }
-
-    public function dispatch(string $uri, string $method): void
-    {
-        $route = $this->findRoute($uri, $method);
-
-        if (! $route) {
-            $this->notFound();
-
-            return;
-        }
-
-        $route->getAction()();
-
-    }
-
-    private function notFound(): void
-    {
-        echo '404 Not Found';
-        exit;
-    }
-
-    private function findRoute(string $uri, string $method): Route|false
-    {
-        if (! isset($this->routes[$method][$uri])) {
-            return false;
-        }
-
-        return $this->routes[$method][$uri];
-    }
-
-    private function initRoutes(): void
-    {
-        $routes = $this->getRoutes();
-        foreach ($routes as $route) {
-            $this->routes[$route->getMethod()][$route->getUri()] = $route;
-        }
-    }
-
-    /**
-     * @return Route[  ]
-     */
-    private function getRoutes(): array
-    {
-        return require_once APP_PATH.'/config/routes.php';
-    }
-}
-
-dd(Route::get('/main', function () {
-    include_once APP_PATH.'/views/pages/main.view.php';
-}));
+dd($toyota);
