7f50fbd88920e6fc5a1e7750ee6e6acc411d4302
ninjamax1917 <ninjamax177@gmail.com>
2025-09-08 21:17:18 +0300

    update

--- about.php
+++ about.php
@@ -1,3 +1,10 @@
-<?php include 'components/head.php'; ?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
     <h1>Это страница about.php!</h1>
 <?php include 'components/footer.php'; ?>
